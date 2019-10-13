c8295116eac96de6bd2e0ae707c7b6696834f548
Colin Liu <colin198702@gmail.com>
2019-10-13 23:16:32 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Colin Liu on 2019/10/13.
+//  Copyright © 2019 Colin Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
